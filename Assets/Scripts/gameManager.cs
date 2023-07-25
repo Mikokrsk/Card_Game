5f757f31939cda1df3ce47fa58b9b6ed1a8e262c
Mykola Krasko <mikokrsk@gmail.com>
2023-07-25 22:30:59 +0300

    Add Game States
    + Menu State
    + Adventure State
    + Battle State
    + Pause State

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -9,8 +9,11 @@ using System;
 
 public class gameManager : MonoBehaviour
 {
-    public static gameManager Instance;    
-   // [SerializeField] private Player playerController;
+    public GameState gameState = GameState.menu;
+    private GameObject BattleCanvas;
+    private GameObject AdventureCanvas;
+    public static gameManager Instance;
+    // [SerializeField] private Player playerController;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -20,8 +23,9 @@ public class gameManager : MonoBehaviour
             return;
         }
         Instance = this;
-        DontDestroyOnLoad(gameObject);        
+        DontDestroyOnLoad(gameObject);
     }
+
     void Start()
     {
         SaveManager.Instance.LoadPlayerData();
@@ -30,7 +34,52 @@ public class gameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (gameState == GameState.battle)
+        {
+            BattleState(true);
+        }
+    }
 
+    private void MenuState(bool active)
+    {
+        if (active)
+        {
+            //Menu activate
+        }
+        else
+        {
+            AdventureState(!active);
+            BattleState(!active);
+        }
+
+    }
+
+    private void AdventureState(bool active)
+    {
+        AdventureCanvas.SetActive(active);
+
+    }
+
+    private void BattleState(bool active)
+    {
+        if (active)
+        {
+            AdventureState(true);
+        }
+        BattleManager.Instance.gameObject.SetActive(active);
+        BattleCanvas.SetActive(active);
+    }
+
+    public void PauseState(bool active)
+    {
+        if (active)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
     }
 
     public void LoadScene(int index)
@@ -38,66 +87,10 @@ public class gameManager : MonoBehaviour
         SceneManager.LoadScene(index);
     }
 
-    /*[System.Serializable]
-    class SaveDataClass
-    {
-        public int health;
-        public int maxHealth;
-        public int armor;
-        public int maxArmor;
-        public int strength;
-        public int agility;
-        public int intelligence;
-        public int endurance;
-        public int money;
-        public int experience;
-        public int blockingPower;
-        public bool isAlife;
-    }
-
-    public void SaveData()
-    {
-        SaveDataClass data = new SaveDataClass();
-        data.health = Player.Instance.health;
-        data.maxHealth = Player.Instance.maxHealth;
-        data.armor = Player.Instance.armor;
-        data.maxArmor = Player.Instance.maxArmor;
-        data.strength = Player.Instance.strength;
-        data.agility = Player.Instance.agility;
-        data.intelligence = Player.Instance.intelligence;
-        data.endurance = Player.Instance.endurance;
-        data.money = Player.Instance.money;
-        data.experience = Player.Instance.experience;
-        data.blockingPower = Player.Instance.blockingPower;
-        data.isAlife = Player.Instance.isAlive;
-        string json = JsonUtility.ToJson(data);
-        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
-        //Debug.Log($"Save Health = {data.health} MaxHealth = {data.maxHealth}");
-    }
-
-    public void LoadData()
-    {
-        string path = Application.persistentDataPath + "/savefile.json";
-        if (File.Exists(path))
-        {
-            string json = File.ReadAllText(path);
-            SaveDataClass data = JsonUtility.FromJson<SaveDataClass>(json);
-
-             Player.Instance.health = data.health;
-             Player.Instance.maxHealth = data.maxHealth;
-             Player.Instance.armor = data.armor;
-             Player.Instance.maxArmor = data.maxArmor;
-             Player.Instance.strength = data.strength;
-             Player.Instance.agility = data.agility;
-             Player.Instance.intelligence = data.intelligence;
-             Player.Instance.endurance = data.endurance;
-             Player.Instance.money = data.money;
-             Player.Instance.experience = data.experience;
-             Player.Instance.blockingPower = data.blockingPower;
-             Player.Instance.isAlive = data.isAlife;
-             Debug.Log(Application.persistentDataPath);
-        }
-        Player.Instance.UpdateHUD();
-    }*/
-
+}
+public enum GameState
+{
+    menu,
+    adventure,
+    battle
 }
